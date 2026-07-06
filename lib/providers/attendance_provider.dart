15f1a0cc5894c65846ae6fd50befffff57c3b232
Ajay Kumar <gsoftranchi@gmail.com>
2026-07-06 16:48:08 +0530

    Sprint 18A.3: Complete Attendance Provider

--- lib/providers/attendance_provider.dart
+++ lib/providers/attendance_provider.dart
@@ -1,14 +1,49 @@
 import 'package:flutter/foundation.dart';
 
+import '../data/repositories/impl/attendance_repository_impl.dart';
 import '../models/attendance_model.dart';
 
 class AttendanceProvider extends ChangeNotifier {
-  final bool _loading = false;
+  AttendanceProvider({
+    AttendanceRepositoryImpl? repository,
+  }) : _repository =
+      repository ??
+          AttendanceRepositoryImpl();
+
+  final AttendanceRepositoryImpl _repository;
+
+  bool _loading = false;
+
+  String? _error;
 
   final List<AttendanceModel> _attendance = [];
 
   bool get loading => _loading;
 
+  String? get error => _error;
+
   List<AttendanceModel> get attendance =>
       List.unmodifiable(_attendance);
+  Future<void> loadAttendance({
+    required String gymId,
+  }) async {
+    _loading = true;
+    _error = null;
+    notifyListeners();
+
+    try {
+      final attendance = await _repository.loadAttendance(
+        gymId: gymId,
+      );
+
+      _attendance
+        ..clear()
+        ..addAll(attendance);
+    } catch (e) {
+      _error = e.toString();
+    } finally {
+      _loading = false;
+      notifyListeners();
+    }
+  }
 }
